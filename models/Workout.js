1cbb818a9f872c4f64a5936b1c95ba69ccb59e3f
Rachel Kroetch <rekroetch@ucdavis.edu>
2021-02-18 13:24:01 -0800

    server routes

--- models/Workout.js
+++ models/Workout.js
@@ -22,4 +22,4 @@ const WorkoutSchema = new Schema({
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
-module.exports ={  Workout };
+module.exports = {  Workout };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -25,7 +26,29 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 //     console.log(message);
 //   });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+// app.get("/exercise", (req, res) => {
+//   db.Workout.find({})
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -35,37 +58,51 @@ app.get("/exercise", (req, res) => {
     });
 });
 
-// app.get("/user", (req, res) => {
-//   db.User.find({})
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
-// app.post("/submit", ({ body }, res) => {
-//   db.Note.create(body)
-//     .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
 
-// app.get("/populateduser", (req, res) => {
-//   db.User.find({})
-//     .populate("notes")
-//     .then(dbUser => {
-//       res.json(dbUser);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+        { _id: req.params.id },
+        { $set: { exercises: req.body } },
+
+        (error, edited) => {
+            if (error) {
+            console.log(error);
+            res.send(error);
+            } else {
+            console.log(edited);
+            res.send(edited);
+            }
+        }
+    );
+    // .then(dbWorkout => {
+    //   res.json(dbWorkout);
+    // })
+    // .catch(err => {
+    //   res.json(err);
+    // });
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $set: { exercises: req.body } }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
